1c2ec67ac2105d902264ba6dbd7c016e039456be
RenanArques <renanarques4448@gmail.com>
2020-04-29 16:23:47 -0300

    Makes the navigation bar responsive

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Icon } from "@iconify/react";
+import barIcon from "@iconify/icons-fe/bar";
+import closeIcon from "@iconify/icons-fe/close";
 import "./styles.css";
 
 import iEstilusLogo from "../../assets/logo.svg";
 
 export default function Header({ links, button }) {
-  return (
-    <nav className="header">
-      <img src={iEstilusLogo} alt="iEstilus Logo" />
+  const [menuOpened, setMenuOpened] = useState(false);
 
-      <div className="links-container">
+  return (
+    <nav className={menuOpened ? "header header-opened" : "header"}>
+      <div className="nav-container">
+        <img src={iEstilusLogo} alt="iEstilus Logo" />
+        <Icon
+          icon={menuOpened ? closeIcon : barIcon}
+          className="menu-open-close-icon"
+          onClick={() => setMenuOpened(!menuOpened)}
+        />
+      </div>
+
+      <div
+        className={
+          menuOpened ? "links-container" : "links-container hide-on-mobile"
+        }
+      >
         {links.map((link, index) => (
-          <span onClick={link.onClick} className="link" key={index}>
+          <span
+            onClick={() => {
+              setMenuOpened(false);
+              link.onClick();
+            }}
+            className="link"
+            key={index}
+          >
             {link.text}
           </span>
         ))}
 
-        <button onClick={button.onClick}>{button.text}</button>
+        <button
+          onClick={() => {
+            setMenuOpened(false);
+            button.onClick();
+          }}
+        >
+          {button.text}
+        </button>
       </div>
     </nav>
   );
