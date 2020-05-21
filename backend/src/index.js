efe166e66ebe29332978ffa81bd43fb747fef0a5
RenanArques <renanarques4448@gmail.com>
2020-05-21 13:36:28 -0300

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 require("dotenv/config");
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./routes");
 
 const app = express();
 
 const port = process.env.PORT || 3030;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
