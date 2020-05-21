81e75f8b55e46fc93a94a003a05c0b893b816319
RenanArques <renanarques4448@gmail.com>
2020-05-21 13:36:52 -0300

    Authenticate User

--- backend/src/controllers/ManagerController.js
+++ backend/src/controllers/ManagerController.js
@@ -4,10 +4,16 @@ module.exports = {
   async create(req, res) {
     const { uid } = req.headers;
 
-    const manager = await connection("managers").insert({
-      firebase_uid: uid,
-    });
+    const manager = await connection("managers").where({ firebase_uid: uid });
 
-    return res.status(201).json(manager);
+    if (manager.length == 0) {
+      const created = await connection("managers").insert({
+        firebase_uid: uid,
+      });
+
+      return res.status(201).json(created && { firebase_uid: uid });
+    }
+
+    return res.status(200).json(manager[0]);
   },
 };
