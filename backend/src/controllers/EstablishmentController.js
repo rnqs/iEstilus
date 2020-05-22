78dcf1147482d4ff8bd6cdad54483d01e7f974d4
RenanArques <renanarques4448@gmail.com>
2020-05-22 13:44:40 -0300

    Pagination and distance order

--- backend/src/controllers/EstablishmentController.js
+++ backend/src/controllers/EstablishmentController.js
@@ -3,31 +3,55 @@ const st = require("../database/postgis");
 
 module.exports = {
   async index(req, res) {
-    if (req.query.uid) {
-      const establishments = await connection("establishments")
-        .select()
-        .where({ firebase_uid: req.query.uid });
+    const { page = 1 } = req.query;
+
+    const [{ count }] = await connection("establishments").count();
+
+    const setDistances = st.distance(
+      st.transform(
+        st.setSRID(
+          st.makePoint(req.query.longitude || 0, req.query.latitude || 0),
+          4326
+        ),
+        3857
+      ),
+      st.transform(st.setSRID("coordinate", 4326), 3857)
+    );
+    try {
+      let establishments = await connection("establishments")
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select([
+          "*",
+          req.query.latitude && req.query.longitude
+            ? setDistances.as("distance")
+            : "*",
+          st.asGeoJSON("coordinate"),
+        ])
+        .whereRaw(
+          req.query.q
+            ? `to_tsvector('portuguese', name || ' ' || description || ' ' || address) @@ to_tsquery('portuguese', '${req.query.q
+                .trim()
+                .replace(/\s/g, " & ")}')`
+            : ``
+        )
+        .orderBy(
+          req.query.latitude && req.query.longitude ? "distance" : "_id"
+        );
+
+      establishments.forEach((establishment) => {
+        const { coordinates } = JSON.parse(establishment.coordinate);
+        establishment.coordinate = {};
+        establishment.coordinate.latitude = coordinates[1];
+        establishment.coordinate.longitude = coordinates[0];
+      });
+
+      res.header("X-Total-count", count);
 
       return res.json(establishments);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
-
-    const [count] = await connection("establishments").count();
-
-    let establishments = await connection("establishments").select([
-      "establishments.*",
-      st.asGeoJSON("coordinate"),
-    ]);
-
-    establishments.forEach((establishment) => {
-      const { coordinates } = JSON.parse(establishment.coordinate);
-      establishment.coordinate = {};
-      establishment.coordinate.latitude = coordinates[1];
-      establishment.coordinate.longitude = coordinates[0];
-    });
-
-    res.header("X-Total-count", count["count(*)"]);
-
-    return res.json(establishments);
   },
 
   async create(req, res) {
