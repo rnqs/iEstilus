c56d102b48aecddd61595b5c78aa5e234e943704
RenanArques <renanarques4448@gmail.com>
2020-05-22 17:17:41 -0300

    Config to Heroku deploy

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,35 +3,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://renanarques:@localhost/iestilus",
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./src/database/migrations",
-    },
-  },
-
-  staging: {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "password",
-      database: "iestilus_db",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./src/database/migrations",
-    },
-  },
-
-  production: {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "password",
-      database: "iestilus_db",
-    },
+    connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
       directory: "./src/database/migrations",
