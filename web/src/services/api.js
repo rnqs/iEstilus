41c40057ec58f2bcec0699825650ae88aaf6e37a
RenanArques <renanarques4448@gmail.com>
2020-05-22 14:23:45 -0300

    Fix axios baseURL env and fix overflow

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:3030",
-});
+const baseURL =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://localhost:3030"
+    : "https://iestilus.herokuapp.com";
+
+const api = axios.create({ baseURL });
 
 export default api;
