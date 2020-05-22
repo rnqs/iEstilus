6eb9e13d040869920cf0e9278d39e8ca327c2efd
RenanArques <renanarques4448@gmail.com>
2020-05-22 17:46:54 -0300

    Fix knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,7 +3,25 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: "postgres://renanarques:@localhost/iestilus",
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./src/database/migrations",
+    },
+  },
+
+  staging: {
+    client: "pg",
+    connection: "postgres://renanarques:@localhost/iestilus",
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./src/database/migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: "postgres://renanarques:@localhost/iestilus",
     useNullAsDefault: true,
     migrations: {
       directory: "./src/database/migrations",
