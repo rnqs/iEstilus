59964b7ccd096d672ef6d9994887b0c693356b7a
RenanArques <renanarques4448@gmail.com>
2020-06-09 19:09:27 -0300

    Fix cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 const port = process.env.PORT || 3030;
 
-app.use(cors());
+const corsOptions = {
+  exposedHeaders: "X-Total-count",
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(routes);
